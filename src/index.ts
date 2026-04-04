6bd0acf79df3825682f7293d1d5bdbc58b4a981e
storycraft <storycraft@pancake.sh>
2026-04-04 16:48:18 +0900

    fix: type decl

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,11 @@
-import type {
-    Beatmap,
-    DifficultyAttrs,
-    DifficultyAttrsData,
-    PerformanceAttrsData,
-    ScoreInfoData,
-    TimedDifficultyAttrsData
-} from '../native/dist/binding';
+/// <reference types="../native/dist/binding" />
 
 const exports = require('../native/dist/binding.node');
 
-export const Beatmap = exports.Beatmap as Beatmap;
-export const DifficultyAttrs = exports.DifficultyAttrs as DifficultyAttrs;
-export const GradualDifficulty = exports.GradualDifficulty as GradualDifficulty;
-export const DifficultyAttrsData = exports.DifficultyAttrsData as DifficultyAttrsData;
-export const PerformanceAttrsData = exports.PerformanceAttrsData as PerformanceAttrsData;
-export const ScoreInfoData = exports.ScoreInfoData as ScoreInfoData;
-export const TimedDifficultyAttrsData = exports.TimedDifficultyAttrsData as TimedDifficultyAttrsData;
+export const Beatmap = exports.Beatmap;
+export const DifficultyAttrs = exports.DifficultyAttrs;
+export const GradualDifficulty = exports.GradualDifficulty;
+export const DifficultyAttrsData = exports.DifficultyAttrsData;
+export const PerformanceAttrsData = exports.PerformanceAttrsData;
+export const ScoreInfoData = exports.ScoreInfoData;
+export const TimedDifficultyAttrsData = exports.TimedDifficultyAttrsData;
