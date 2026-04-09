fcc76a4715b4bd1be689dfb524e5590d3f8e71dc
storycraft <storycraft@pancake.sh>
2026-04-09 17:53:23 +0900

    feat: set strains value for each type and include without slider variant

--- lib/native/Data/StrainsData.cs
+++ lib/native/Data/StrainsData.cs
@@ -11,39 +11,83 @@ using osu.Game.Rulesets.Taiko.Difficulty.Skills;
 namespace binding.Data;
 
 [JSExport]
-public readonly struct StrainsData
+public struct StrainsData
 {
     #region osu!
-    public required Memory<double> Aim { get; init; }
-    public required Memory<double> Flashlight { get; init; }
-    public required Memory<double> Speed { get; init; }
+    public Memory<double> Aim { get; set; }
+    public Memory<double> AimWithoutSliders { get; set; }
+    public Memory<double> Flashlight { get; set; }
+    public Memory<double> Speed { get; set; }
     #endregion
 
     #region osu!taiko
-    public required Memory<double> Color { get; init; }
-    public required Memory<double> Rhythm { get; init; }
-    public required Memory<double> Reading { get; init; }
-    public required Memory<double> Stamina { get; init; }
+    public Memory<double> Color { get; set; }
+    public Memory<double> Rhythm { get; set; }
+    public Memory<double> Reading { get; set; }
+    public Memory<double> Stamina { get; set; }
     #endregion
 
     #region osu!catch
-    public required Memory<double> Movement { get; init; }
+    public Memory<double> Movement { get; set; }
     #endregion
 
     #region osu!mania
-    public required Memory<double> Strains { get; init; }
+    public Memory<double> Strains { get; set; }
     #endregion
 
-    internal static StrainsData FromSkills(Skill[] skills) => new()
+    private void SetStrains(StrainSkill skill)
     {
-        Aim = skills.OfType<Aim>().FirstOrDefault()?.GetObjectStrains().ToArray() ?? [],
-        Flashlight = skills.OfType<Flashlight>().FirstOrDefault()?.GetObjectStrains().ToArray() ?? [],
-        Speed = skills.OfType<Speed>().FirstOrDefault()?.GetObjectStrains().ToArray() ?? [],
-        Color = skills.OfType<Colour>().FirstOrDefault()?.GetObjectStrains().ToArray() ?? [],
-        Reading = skills.OfType<Reading>().FirstOrDefault()?.GetObjectStrains().ToArray() ?? [],
-        Rhythm = skills.OfType<Rhythm>().FirstOrDefault()?.GetObjectStrains().ToArray() ?? [],
-        Stamina = skills.OfType<Stamina>().FirstOrDefault()?.GetObjectStrains().ToArray() ?? [],
-        Movement = skills.OfType<Movement>().FirstOrDefault()?.GetObjectStrains().ToArray() ?? [],
-        Strains = skills.OfType<Strain>().FirstOrDefault()?.GetObjectStrains().ToArray() ?? [],
-    };
+        var strains = skill.GetObjectStrains().ToArray();
+        switch (skill)
+        {
+            case Aim aim:
+                if (aim.IncludeSliders)
+                {
+                    Aim = strains;
+                }
+                else
+                {
+                    AimWithoutSliders = strains;
+                }
+                break;
+            case Flashlight _:
+                Flashlight = strains;
+                break;
+            case Speed _:
+                Speed = strains;
+                break;
+            case Colour _:
+                Color = strains;
+                break;
+            case Reading _:
+                Reading = strains;
+                break;
+            case Rhythm _:
+                Rhythm = strains;
+                break;
+            case Stamina _:
+                Stamina = strains;
+                break;
+            case Movement _:
+                Movement = strains;
+                break;
+            case Strain _:
+                Strains = strains;
+                break;
+        }
+    }
+
+    internal static StrainsData FromSkills(Skill[] skills)
+    {
+        var data = new StrainsData();
+        foreach (var skill in skills)
+        {
+            if (skill is StrainSkill strainSkill)
+            {
+                data.SetStrains(strainSkill);
+            }
+        }
+
+        return data;
+    }
 }
