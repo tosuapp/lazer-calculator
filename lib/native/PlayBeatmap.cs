addc24999e5796b97c1863a0881c223f4710ace5
cyperdark <21735205+cyperdark@users.noreply.github.com>
2026-09-24 17:13:35 +0300

    chore: rename function

--- lib/native/PlayBeatmap.cs
+++ lib/native/PlayBeatmap.cs
@@ -79,7 +79,7 @@ public class PlayBeatmap
     /// <summary>
     /// Check if given or current mods combination is rankable.
     /// </summary>
-    public bool CanModsRanked(LazerMod[]? mods)
+    public bool IsModsRankable(LazerMod[]? mods)
     {
         var list = mods?.Select(m => m.ToMod(ruleset)) ?? Mods;
         return list.All(mod => mod.Ranked);
